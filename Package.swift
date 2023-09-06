96b7ee32ac419fca9b064acb7258f4783a15f02c
marcprux <mwp1@cornell.edu>
2023-09-06 08:13:05 -0400

    Modernize package

--- Package.swift
+++ Package.swift
@@ -7,28 +7,15 @@ let package = Package(
     platforms: [.iOS(.v16), .macOS(.v13), .tvOS(.v16), .watchOS(.v9), .macCatalyst(.v16)],
     products: [
         .library(name: "SkipSQL", targets: ["SkipSQL"]),
-        .library(name: "SkipSQLKt", targets: ["SkipSQLKt"]),
     ],
     dependencies: [
-        .package(url: "https://source.skip.tools/skip.git", from: "0.6.28"),
-        .package(url: "https://source.skip.tools/skip-unit.git", from: "0.2.4"),
-        .package(url: "https://source.skip.tools/skip-lib.git", from: "0.3.4"),
-        .package(url: "https://source.skip.tools/skip-foundation.git", from: "0.1.4"),
+        .package(url: "https://source.skip.tools/skip.git", from: "0.6.39"),
+        .package(url: "https://source.skip.tools/skip-foundation.git", from: "0.1.9"),
     ],
     targets: [
-        .target(name: "SkipSQL", plugins: [.plugin(name: "skippy", package: "skip")]),
-        .target(name: "SkipSQLKt", dependencies: [
-            "SkipSQL",
-            .product(name: "SkipUnitKt", package: "skip-unit"),
-            .product(name: "SkipLibKt", package: "skip-lib"),
-            .product(name: "SkipFoundationKt", package: "skip-foundation"),
-        ], resources: [.process("Skip")], plugins: [.plugin(name: "transpile", package: "skip")]),
+        .target(name: "SkipSQL", dependencies: [.product(name: "SkipFoundation", package: "skip-foundation", condition: .when(platforms: [.macOS]))], plugins: [.plugin(name: "skipstone", package: "skip")]),
         .testTarget(name: "SkipSQLTests", dependencies: [
             "SkipSQL"
-        ], plugins: [.plugin(name: "skippy", package: "skip")]),
-        .testTarget(name: "SkipSQLKtTests", dependencies: [
-            "SkipSQLKt",
-            .product(name: "SkipUnit", package: "skip-unit"),
-        ], resources: [.process("Skip")], plugins: [.plugin(name: "transpile", package: "skip")]),
+        ], plugins: [.plugin(name: "skipstone", package: "skip")]),
     ]
 )
