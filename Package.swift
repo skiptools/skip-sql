21cff08c986837e312b23d6bafb064695ff25962
marcprux <mwp1@cornell.edu>
2024-02-05 16:36:02 -0500

    Use gradle version catalogs for dependencies

--- Package.swift
+++ Package.swift
@@ -9,9 +9,9 @@ let package = Package(
         .library(name: "SkipSQL", targets: ["SkipSQL"]),
     ],
     dependencies: [
-        .package(url: "https://source.skip.tools/skip.git", from: "0.7.36"),
-        .package(url: "https://source.skip.tools/skip-foundation.git", from: "0.3.11"),
-        .package(url: "https://source.skip.tools/skip-ffi.git", from: "0.2.2"),
+        .package(url: "https://source.skip.tools/skip.git", from: "0.8.0"),
+        .package(url: "https://source.skip.tools/skip-foundation.git", from: "0.5.0"),
+        .package(url: "https://source.skip.tools/skip-ffi.git", from: "0.3.0"),
     ],
     targets: [
         .target(name: "SkipSQL", dependencies: [
