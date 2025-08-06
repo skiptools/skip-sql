cc4b8a87d1d95e085b01c5b3d2939e0c021e2bec
Marc Prud'hommeaux <mwp1@cornell.edu>
2025-08-06 12:24:56 +0200

    Add exported import SkipSQL to SkipSQLPlus [no ci]

--- Sources/SkipSQLPlus/SQLPlus.swift
+++ Sources/SkipSQLPlus/SQLPlus.swift
@@ -1,6 +1,6 @@
 // Copyright 2023–2025 Skip
 // SPDX-License-Identifier: LGPL-3.0-only WITH LGPL-3.0-linking-exception
-import SkipSQL
+@_exported import SkipSQL
 
 extension SQLiteConfiguration {
     /// The platform-provided SQLite library.
