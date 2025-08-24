82f6009ad28e8deb0140b2a5374c97419049e599
Marc Prud'hommeaux <mwp1@cornell.edu>
2025-08-23 22:00:48 -0400

    import XCTest

--- Tests/SkipSQLTests/SQLContextTestConfiguration.swift
+++ Tests/SkipSQLTests/SQLContextTestConfiguration.swift
@@ -1,6 +1,7 @@
 // Copyright 2023–2025 Skip
 // SPDX-License-Identifier: LGPL-3.0-only WITH LGPL-3.0-linking-exception
 import SkipSQL
+import XCTest
 
 extension SQLiteConfiguration {
     /// The shared SQLContextTests uses thus local variable to determine the configuration to use
