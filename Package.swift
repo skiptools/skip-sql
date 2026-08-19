f445938c61bb0165ab2af1fba363bf203ab41c5f
Marc Prud'hommeaux <mwp1@cornell.edu>
2026-08-19 13:29:53 -0400

    Change source.skip.tools references to github.com/skiptools

--- Package.swift
+++ Package.swift
@@ -10,10 +10,10 @@ let package = Package(
         .library(name: "SkipSQLPlus", type: .dynamic, targets: ["SkipSQLPlus"]),
     ],
     dependencies: [
-        .package(url: "https://source.skip.tools/skip.git", from: "1.1.11"),
-        .package(url: "https://source.skip.tools/skip-foundation.git", from: "1.1.11"),
-        .package(url: "https://source.skip.tools/skip-unit.git", from: "1.0.1"),
-        .package(url: "https://source.skip.tools/skip-ffi.git", from: "1.0.0"),
+        .package(url: "https://github.com/skiptools/skip.git", from: "1.1.11"),
+        .package(url: "https://github.com/skiptools/skip-foundation.git", from: "1.1.11"),
+        .package(url: "https://github.com/skiptools/skip-unit.git", from: "1.0.1"),
+        .package(url: "https://github.com/skiptools/skip-ffi.git", from: "1.0.0"),
     ],
     targets: [
         .target(name: "SkipSQLCore", dependencies: [
